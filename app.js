256bef3997043280e19e2a470ef076efd586f200
Katya Mikanov <kategubko@gmail.com>
2023-04-20 18:57:20 +0300

    refactor: change class names according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,19 +32,25 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
+
+    listItem.className="li";
+
+    checkBox.className="input-checkbox";
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task li-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task li-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
